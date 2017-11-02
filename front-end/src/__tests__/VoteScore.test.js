79f7f123e59700a54cad74f1206c1ab8932fe082
brolam <breno@brolam.com.br>
2017-11-02 12:47:56 -0300

    test: TDD VoteScore - on click event.

--- front-end/src/__tests__/VoteScore.test.js
+++ front-end/src/__tests__/VoteScore.test.js
@@ -1,26 +1,29 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 import renderer from 'react-test-renderer'
-import { shallow } from 'enzyme';
+import { mount } from 'enzyme';
 import VoteScore from '../components/VoteScore'
 
 test('render without error', () => {
-  ReactDOM.render(
-    <VoteScore
-      entityType="post"
-      entityId="8xf0y6ziyjabvozdd253nd"
-      voteScore={5}
-    />, document.createElement('div'));
+  ReactDOM.render(<VoteScore voteScore={5} />, document.createElement('div'));
 })
 
 test('last Snapshot', () => {
-  const voteScore = renderer.create(
-    <VoteScore
-      entityType="post"
-      entityId="8xf0y6ziyjabvozdd253nd"
-      voteScore={5}
-    />
-  );
+  const voteScore = renderer.create(<VoteScore voteScore={5} />);
   let tree = voteScore.toJSON();
   expect(tree).toMatchSnapshot();
+})
+
+test('on click liked button', () => {
+  let isClickedLikedButton = false;
+  const voteScore = mount(<VoteScore voteScore={5} onClickLiked={e => { isClickedLikedButton = true; }} />);
+  voteScore.find('button [className="liked"]').simulate('click')
+  expect(isClickedLikedButton).toBe(true);
+})
+
+test('on click not liked button', () => {
+  let isClickedNotLikedButton = false;
+  const voteScore = mount(<VoteScore voteScore={5} onClickNotLiked={e => { isClickedNotLikedButton = true; }} />);
+  voteScore.find('button [className="not-liked"]').simulate('click')
+  expect(isClickedNotLikedButton).toBe(true);
 })
--- front-end/src/components/VoteScore.js
+++ front-end/src/components/VoteScore.js
@@ -1,17 +1,25 @@
 import React from 'react'
 import PropTypes from 'prop-types'
 
-function VoteScore(props){
-    return([
-      <button key="buttonLiked" className="liked" href="/"  >Liked <span>{props.voteScore}</span></button>,
-      <button key="buttonNotLiked" className="not-liked" href="/" >Not like</button>
-    ])
-} 
+function VoteScore(props) {
+  return ([
+    <button
+      key="buttonLiked"
+      className="liked"
+      onClick={props.onClickLiked} >
+      Liked <span>{props.voteScore}</span>
+    </button>,
+    <button
+      key="buttonNotLiked"
+      className="not-liked"
+      onClick={props.onClickNotLiked} >
+      Not like
+    </button>
+  ])
+}
 
 VoteScore.propTypes = {
-  entityType: PropTypes.string.isRequired,
-  entityId: PropTypes.string.isRequired,
-  voteScore: PropTypes.number.isRequired
+  voteScore: PropTypes.number.isRequired,
 }
 
 export default VoteScore
