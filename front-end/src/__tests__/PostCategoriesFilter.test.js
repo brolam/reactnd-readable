df5e4e838145690468b77aec183aa874c4d38911
brolam <breno@brolam.com.br>
2017-11-02 16:28:34 -0300

    test: TDD PostCategoriesFilter - last Snapshot.

--- front-end/src/__tests__/PostCategoriesFilter.test.js
+++ front-end/src/__tests__/PostCategoriesFilter.test.js
@@ -1,6 +1,7 @@
 
 import React from 'react'
 import ReactDOM from 'react-dom'
+import renderer from 'react-test-renderer'
 import { mount } from 'enzyme'
 import PostCategoriesFilter from '../components/PostCategoriesFilter'
 
@@ -10,7 +11,6 @@ test('render without error', () => {
 })
 
 test('on selected a category', () => {
-
   const testOnSelectOneCategory = (category) => {
     const onSelectCategory = (e) => {
       const selectedCategory = e.target.value;
@@ -21,12 +21,22 @@ test('on selected a category', () => {
         categories={categories}
         onSelected={onSelectCategory}
       />);
-    const event = {target: {value: category}}; 
+    const event = { target: { value: category } };
     postCategoriesFilter.find('select').simulate('change', event);
   };
   testOnSelectOneCategory("udacity");
   testOnSelectOneCategory("reactd");
   testOnSelectOneCategory("redux");
+})
+
+test('last Snapshot', () => {
+  const postCategoriesFilter = renderer.create(
+    <PostCategoriesFilter
+      categories={categories}
+      onSelected={e =>{}}
+    />);
+  let tree = postCategoriesFilter.toJSON();
+  expect(tree).toMatchSnapshot();
 })
 
 const categories = ['udacity', 'react', 'redux']
