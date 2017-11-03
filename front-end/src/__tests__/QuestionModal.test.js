48cff53638cfdf2e9090863c90f9e2a04002b4ea
brolam <breno@brolam.com.br>
2017-11-03 20:52:52 -0300

    test: TDD QuestionModal - last Snapshot.

--- front-end/src/__tests__/QuestionModal.test.js
+++ front-end/src/__tests__/QuestionModal.test.js
@@ -1,8 +1,25 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
+import renderer from 'react-test-renderer'
 import QuestionModal from '../components/QuestionModal'
 
 test('render without error', () => {
   ReactDOM.render(
-    <QuestionModal/>, document.createElement('div'));
+    <QuestionModal
+      message={"A question was render without error"}
+      onYesAnswer={e => { }}
+      onNoAnswer={e => { }}
+    />, document.createElement('div'));
+})
+
+
+test('last Snapshot', () => {
+  const questionModal = renderer.create(
+    <QuestionModal
+      message={"Question?"}
+      onYesAnswer={e => { }}
+      onNoAnswer={e => { }}
+    />);
+  let tree = questionModal.toJSON();
+  expect(tree).toMatchSnapshot();
 })
--- front-end/src/components/QuestionModal.js
+++ front-end/src/components/QuestionModal.js
@@ -1,9 +1,23 @@
 import React from 'react'
+import PropTypes from 'prop-types'
 
 function QuestionModal(props) {
+  const { message, onYesAnswer, onNoAnswer } = props
   return (
-    <div></div>
+    <div className="modal-short">
+      <div className="modal-short-dialog">
+        <h1>
+          {message} <a className="yes" href="/">Yes?</a>  or  <a className="no" href="/">No?</a>
+        </h1>
+      </div>
+    </div>
   )
+}
+
+QuestionModal.propTypes = {
+  message: PropTypes.string.isRequired,
+  onYesAnswer: PropTypes.func.isRequired,
+  onNoAnswer: PropTypes.func.isRequired
 }
 
 export default QuestionModal
