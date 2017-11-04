179e4a3902e81f2700264df24438616af337274a
brolam <breno@brolam.com.br>
2017-11-04 19:56:18 -0300

    test: TDD Home - render without error.

---
+++ front-end/src/__tests__/Home.test.js
@@ -0,0 +1,32 @@
+import React from 'react'
+import ReactDOM from 'react-dom'
+import Home from '../components/Home'
+
+
+test('render without error', () => {
+  ReactDOM.render(
+    <Home posts={posts} />,
+    document.createElement('div'));
+})
+
+const posts = [{
+  category: "udacity",
+  id: "7ni6ok3ym7mf1p33lnez",
+  title: "Udacity is the best place to learn technology.",
+  timestamp: 1467166872634,
+  author: "thingtwo",
+  body: "Everyone says so after all.",
+  deleted: false,
+  voteScore: 5
+},
+{
+  category: "react",
+  id: "8xf0y6ziyjabvozdd253nd",
+  title: "Udacity is the best place to learn React",
+  timestamp: 1467166872634,
+  author: "thingtwo",
+  body: "Everyone says so after all.",
+  deleted: false,
+  voteScore: 5
+}]
+
---
+++ front-end/src/components/Home.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function Home({ posts }) {
+  return (
+    <div></div>
+  )
+}
+
+export default Home
