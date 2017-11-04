6d6af2fd593691c151d64cf4e86ab40fb62731a6
brolam <breno@brolam.com.br>
2017-11-04 13:03:22 -0300

    test: TDD QuestionModal - when clicking anywhere and not on yes or no.

--- front-end/src/__tests__/QuestionModal.test.js
+++ front-end/src/__tests__/QuestionModal.test.js
@@ -1,6 +1,7 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 import renderer from 'react-test-renderer'
+import { mount } from 'enzyme'
 import QuestionModal from '../components/QuestionModal'
 
 test('render without error', () => {
@@ -22,4 +23,16 @@ test('last Snapshot', () => {
     />);
   let tree = questionModal.toJSON();
   expect(tree).toMatchSnapshot();
+})
+
+test('when clicking anywhere and not on yes or no', () => {
+  let isClikedNoAnswer = false;
+  const questionModal = mount(
+    <QuestionModal
+      message={"Question?"}
+      onYesAnswer={e => { }}
+      onNoAnswer={e => { isClikedNoAnswer = true }}
+    />);
+  questionModal.find('div [className="modal-short"]').simulate('click')
+  expect(isClikedNoAnswer).toBe(true);
 })
--- front-end/src/components/QuestionModal.js
+++ front-end/src/components/QuestionModal.js
@@ -4,7 +4,7 @@ import PropTypes from 'prop-types'
 function QuestionModal(props) {
   const { message, onYesAnswer, onNoAnswer } = props
   return (
-    <div className="modal-short">
+    <div className="modal-short" onClick={onNoAnswer}>
       <div className="modal-short-dialog">
         <h1>
           {message} <a className="yes" href="/">Yes?</a>  or  <a className="no" href="/">No?</a>
