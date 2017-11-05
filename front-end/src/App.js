da73f7af6bfaa7757d03235c905d4b72d9354c1c
brolam <breno@brolam.com.br>
2017-11-05 12:35:35 -0300

    refactor: App added Post component.

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -1,12 +1,7 @@
 import React, { Component } from 'react';
 import './App.css';
-import VoteScore from './components/VoteScore'
-import PostModal from './components/PostModal'
-import PostCommentModal from './components/PostCommentModal'
-import QuestionModal from './components/QuestionModal'
 import Home from './components/Home'
-import PostCommentList from './components/PostCommentList'
-import PostCommentBody from './components/PostCommentBody'
+import Post from './components/Post'
 
 class App extends Component {
 
@@ -27,84 +22,23 @@ class App extends Component {
   render() {
     const { url, selectedPost, posts, categories, comments } = this.state
     if (url === "home") {
-      return (<Home posts={posts} categories={categories} />)
+      return (
+        <Home
+          posts={posts}
+          categories={categories}
+        />)
     } else if (url === "post") {
-      return this.getPost(selectedPost, comments.filter(comment => (comment.parentId === selectedPost.id)))
+      return (
+        <Post
+          post={selectedPost}
+          comments={comments.filter(comment => (comment.parentId === selectedPost.id))}
+        />
+      )
     }
   }
   componentDidMount() { }
 
   componentDidUpdate() { }
-
-  getPost(post, comments) {
-    return (
-      <div className="app">
-        <div className="post-page-header">
-          <a className="post-page-header-back"
-            onClick={e => {
-              this.setState({ url: "home" })
-            }}>Back
-          </a>
-          <div className="post-page-header-title">
-            <span className={post.category} >{post.category}</span>
-            <span>{post.title}</span>
-          </div>
-          <div className="post-page-header-buttons">
-            <button className="edit-button" href="/"
-              onClick={e => {
-                this.setState({ isEditPost: true })
-              }}>Edit</button>
-            <button className="delete-button"
-              onClick={e => {
-                this.setState({ showDeletePostQuestionModal: true })
-              }}>Delete</button>
-            <VoteScore voteScore={post.voteScore} />
-          </div>
-        </div>
-        <div className="post-content">
-          <PostCommentBody post={post} />
-          <PostCommentList post={post} comments={comments} />
-        </div>
-        {
-          (this.state.isNewComment || this.state.isEditComment) && (
-            <PostCommentModal
-              post={post}
-              comment={this.state.isEditComment ? this.state.isEditComment : {}}
-              onClickBackButton={e => this.setState({ isNewComment: false, isEditComment: null })}
-              onSave={e => this.setState({ isNewComment: false, isEditComment: null })}
-
-            />
-          )}
-        {
-          this.state.showDeletePostQuestionModal && (
-            <QuestionModal
-              message={"Are you sure? Do you want delete this post? "}
-              timeout={7000}
-              onYesAnswer={e => this.setState({ showDeletePostQuestionModal: false })}
-              onNoAnswer={e => this.setState({ showDeletePostQuestionModal: false })}
-            />
-          )}
-        {
-          this.state.showDeleteCommentQuestionModal && (
-            <QuestionModal
-              message={"Are you sure? Do you want delete this comment?"}
-              timeout={7000}
-              onYesAnswer={e => this.setState({ showDeleteCommentQuestionModal: false })}
-              onNoAnswer={e => this.setState({ showDeleteCommentQuestionModal: false })}
-            />
-          )}
-        {
-          this.state.isEditPost && (
-            <PostModal
-              post={post}
-              categories={this.state.categories}
-              onClickBackButton={e => { this.setState({ isEditPost: false }) }}
-              onSave={e => { this.setState({ isEditPost: false }) }}
-            />
-          )}
-      </div>
-    )
-  }
 }
 
 const postUdacity = {
