5320dbf6da4da7437424149cca9e236d7031f441
brolam <breno@brolam.com.br>
2017-11-07 18:09:38 -0300

    test: TDD Home - last Snapshot - show new post form modal.

--- front-end/src/__tests__/Home.test.js
+++ front-end/src/__tests__/Home.test.js
@@ -4,6 +4,7 @@ import renderer from 'react-test-renderer'
 import Home from '../components/Home'
 
 const categories = global.dataForTest.categories
+const posts = global.dataForTest.posts
 
 test('render without error', () => {
   ReactDOM.render(
@@ -13,6 +14,12 @@ test('render without error', () => {
 
 test('last Snapshot', () => {
   const home = renderer.create(<Home posts={posts} categories={categories} />);
+  let tree = home.toJSON();
+  expect(tree).toMatchSnapshot();
+})
+
+test('last Snapshot - show new post form modal', () => {
+  const home = renderer.create(<Home isNewPost={true} posts={posts} categories={categories} />);
   let tree = home.toJSON();
   expect(tree).toMatchSnapshot();
 })
@@ -38,25 +45,3 @@ test('last Snapshot - show error message modal', () => {
   let tree = home.toJSON();
   expect(tree).toMatchSnapshot();
 })
-
-const posts = [{
-  category: "udacity",
-  id: "7ni6ok3ym7mf1p33lnez",
-  title: "Udacity is the best place to learn technology.",
-  timestamp: 1467166872634,
-  author: "thingtwo",
-  body: "Everyone says so after all.",
-  deleted: false,
-  voteScore: 5
-},
-{
-  category: "react",
-  id: "8xf0y6ziyjabvozdd253nd",
-  title: "Udacity is the best place to learn React",
-  timestamp: 1467166872634,
-  author: "thingtwo",
-  body: "Everyone says so after all.",
-  deleted: false,
-  voteScore: 5
-}]
-
