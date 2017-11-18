8a1488baaf4556c899a77f1df6951dbd7650c14f
brolam <breno@brolam.com.br>
2017-11-18 10:57:27 -0300

    test: TDD SearchBar - debounce input onSearch.

--- front-end/src/__tests__/SearchBar.test.js
+++ front-end/src/__tests__/SearchBar.test.js
@@ -1,8 +1,42 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
-
+import renderer from 'react-test-renderer'
+import { mount } from 'enzyme'
 import SearchBar from '../components/SearchBar'
 
 test('render without error', () => {
   ReactDOM.render(<SearchBar placeholder="Search by title post" />, document.createElement('div'));
 })
+
+test('last Snapshot', () => {
+  const searchBar = renderer.create(
+    <SearchBar
+      placeholder="Search by title post"
+    />
+  );
+  let tree = searchBar.toJSON();
+  expect(tree).toMatchSnapshot();
+})
+
+test('debounce input onSearch', () => {
+  jest.useFakeTimers();
+  let searchValue = undefined;
+  const onSearch = (value) => {
+    searchValue = value
+  }
+  const searchBar = mount(
+    <SearchBar
+      placeholder="Search by title post"
+      onSearch={onSearch}
+    />
+  );
+  const searchBarInput = searchBar.find('input')
+  const event = { target: { value: 'udacity' } };
+  searchBarInput.simulate('change', event);
+  expect(searchValue).toBe(undefined)
+  setTimeout(() => {
+    expect(searchValue).toBe('udacity')
+  }, 3000);
+  jest.runAllTimers();
+})
+
--- front-end/src/components/SearchBar.js
+++ front-end/src/components/SearchBar.js
@@ -1,11 +1,16 @@
 import React from 'react'
+import { DebounceInput } from 'react-debounce-input';
 
-function SearchBar(props) {
+function SearchBar({
+  placeholder,
+  onSearch = value => { console.log(value) } }) {
   return (
     <div className="search-bar">
-      <input
-        type="text"
-        placeholder={props.placeholder}
+      <DebounceInput
+        placeholder={placeholder}
+        minLength={2}
+        debounceTimeout={300}
+        onChange={event => onSearch(event.target.value)}
       />
     </div>
   )
