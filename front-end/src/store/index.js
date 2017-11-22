6759bf7dd95740771243646704baecb7d55217b7
brolam <breno@brolam.com.br>
2017-11-22 07:23:38 -0300

    feat: added redux-logger.

--- front-end/src/store/index.js
+++ front-end/src/store/index.js
@@ -1,5 +1,12 @@
 import reducer from './reducers'
-import { createStore, applyMiddleware } from 'redux'
-import {appMiddleware} from './middlewares'
+import { createStore, applyMiddleware , compose } from 'redux'
+import { appMiddleware } from './middlewares'
+import { createLogger } from 'redux-logger'
 
-export default createStore(reducer,  applyMiddleware(appMiddleware));
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+
+const logger = createLogger({
+  // ...options
+});
+
+export default createStore(reducer,  composeEnhancers(applyMiddleware(appMiddleware, logger)));
